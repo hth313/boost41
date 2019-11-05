8ccbf808226fc666f9b1467d39b24f7168c2fbf9
Håkan Thörngren <hth313@gmail.com>
2019-11-04 20:05:47 -0800

    Remove to power off poll vector
    It is no longer needed as there is no X-memory cache feature, it was
    considered to be too dangerous.

--- src/linker.scm
+++ src/linker.scm
@@ -2,6 +2,6 @@
   '((memory Boost (position independent)
             (bank 1) (address (#x0 . #xFFF))
             (section (BoostFAT #x0) BoostCode Lib41Code
-                     BoostTable RPN (BoostPoll #xFE2))
+                     BoostTable RPN (BoostPoll #xFE4))
             (checksum #xFFF hp41)
             (fill 0))))
--- src/poll.s
+++ src/poll.s
@@ -17,34 +17,16 @@
 deepWake:     n=c
               ldi     .low12 catShell ; activate CAT replacement
               gosub   activateShell
-              goto    10$           ; (P+1) failed, not enough memory
+              goto    pollReturn    ; (P+1) failed, not enough memory
                                     ; (P+2) success
               ldi     .low12 extensionHandlers
               gosub   activateShell
-              goto    10$           ; (P+1) failed, not enough memory
+              goto    pollReturn    ; (P+1) failed, not enough memory
                                     ; (P+2) success
-10$:          gosub   LDSST0
+pollReturn:   gosub   LDSST0
               c=n
               golong  RMCK10
 
-
-;;; **********************************************************************
-;;;
-;;; powerOff - poll vector called at power off
-;;;
-;;; As we are a user of direct X-memory data operations, we log out that
-;;; system to ensure the proper integrity of the system. What happens is
-;;; that we restore the '169' cold start constant that may be off due to
-;;; being used for caching the current file. As mainframe will call MEMCHK
-;;; after this poll vector, we do not want anything but '169' in that slot.
-;;; This also ensures that the cold start constant is in place in case
-;;; Ratatosk is unplugged while power down and acts as the intended safe
-;;; guard to detect memory loss during power off.
-;;;
-;;; **********************************************************************
-
-powerOff:  ;   gosub   logoutXMem
-              golong  RMCK10
 
 ;;; **********************************************************************
 ;;;
@@ -55,7 +37,7 @@ powerOff:  ;   gosub   logoutXMem
               .con    0             ; Pause
               .con    0             ; Running
               .con    0             ; Wake w/o key
-              goto    powerOff      ; Powoff
+              .con    0             ; Powoff
               .con    0             ; I/O
               goto    deepWake      ; Deep wake-up
               goto    deepWake      ; Memory lost
