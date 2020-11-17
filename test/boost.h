20c61762323a90e099065bb69bcc45cb60e1b398
Håkan Thörngren <hth313@gmail.com>
2020-11-17 15:16:24 -0800

    Add a starting point for tests

---
+++ test/boost.h
@@ -0,0 +1,11 @@
+#ifndef __BOOST_H
+#define __BOOST_H
+
+.import boost
+.import 41cx
+
+.postfix PUSH, POP, INC, DEC, ARCLINT, DELAY, KEY (semi-merged-postfix)
+.postfix XRCL, XSTO, XVIEW, XARCL  (semi-merged-postfix)
+.postfix <>, <>X, =, ≠, <, <= (semi-merged-dual)
+
+#endif // __BOOST_H
---
+++ test/boostTest.s
@@ -0,0 +1,26 @@
+#include "mainframe.h"
+
+              .section ID
+              .con    18            ; XROM
+              .con    .fatsize FatEnd ; number of entry points
+              .fat    Header
+
+              .section FATEND
+FatEnd:       .con    0, 0
+
+
+              .section Code
+              .name "-BOOST T 1A"
+Header:       rtn
+
+
+              .section TAIL
+              .con    0             ; Pause
+              .con    0             ; Running
+              .con    0             ; Wake w/o key
+              .con    0             ; Powoff
+              .con    0             ; I/O
+              .con    0             ; Deep wake-up
+              .con    0             ; Memory lost
+              .text   "A1TB"        ; Identifier LT-1A
+              .con    0             ; checksum position
